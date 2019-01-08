1e7f821af87830a748275c6cbcdae2bd569b2932
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2019-01-08 11:42:59 +0100

    Set basic structure.

---
+++ src/main/java/com/codecool/Server.java
@@ -0,0 +1,24 @@
+package com.codecool;
+
+import java.util.List;
+
+public class Server {
+    private List<Peer> activePeers;
+
+
+
+    private void checkActivePeers() {
+        //TODO
+    }
+    public List<String> getFileNameProposition(String fileNamePart) {
+        //TODO
+    }
+
+    public List<Peer> getPeerListForFile(String fileName) {
+        //TODO
+    }
+
+    public List<Peer> getActivePeers() {
+        return activePeers;
+    }
+}
