f5d67613cb2066388eeb7e8edbb5bd47e93824df
Elzacodecool <elliegolec@gmail.com>
2019-01-08 12:13:11 +0100

    Implement getFileNamesFromFolder.

--- src/main/java/com/codecool/Peer.java
+++ src/main/java/com/codecool/Peer.java
@@ -1,5 +1,7 @@
 package com.codecool;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Peer {
@@ -17,7 +19,28 @@ public class Peer {
         return true;
     }
 
+
     public List<String> getFileNamesFromFolder() {
-        return null;
+        return getFileNamesFromFolder(directory);
+    }
+
+    private List<String> getFileNamesFromFolder(String folder) {
+        List<String> fileNames = new ArrayList<>();
+
+        File folderFile = new File(folder);
+        File[] listOfFiles = folderFile.listFiles();
+
+        if (listOfFiles == null) {
+            return fileNames;
+        }
+
+        for (File file : listOfFiles) {
+            if (file.isFile()) {
+                fileNames.add(file.getName());
+            } else if (file.isDirectory()) {
+                fileNames.addAll(getFileNamesFromFolder(file.getName()));
+            }
+        }
+        return fileNames;
     }
 }
